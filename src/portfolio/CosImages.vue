3c1ddd126bfa630574a03e87bbb338b9c299eaed
emily reno <emilyreno@emilys-MacBook-Pro.local>
2019-01-10 09:30:41 -0500

    added cosplay images to mobile portfolio

--- src/portfolio/CosImages.vue
+++ src/portfolio/CosImages.vue
@@ -1,24 +1,32 @@
 <template>
     <div>
-        <mq-layout mq="sm" class="sm-portfolio">
-            <img src="../portfolio/cosplay/groupshot1.png" alt=" group shot" />
+        <mq-layout mq="sm">
+            <Images class="sm-portfolio" />
         </mq-layout>
     </div>
 </template>
 
 <script>
+import Images from '../portfolio/cosplay/Images.vue'
+
 export default {
-    name: 'CosImages'
+    name: 'CosImages',
+    components: {
+        Images
+    }
 }
 </script>
 
 <style scope>
     .sm-portfolio {
         display: flex;
+        flex-direction: column;
         justify-content: center;
     }
     .sm-portfolio img {
         width: 95%;
         align-self: center;
+        margin-bottom: .5em;
+        border: .5em solid #111;
     }
 </style>
---
+++ src/portfolio/cosplay/Images.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img src="../cosplay/groupshot1.png" alt=" group shot" />
+        <img src="../cosplay/camie1.png" alt="camie" />
+        <img src="../cosplay/ochaco1.png" alt="ochaco" />
+        <img src="../cosplay/toga2.png" alt="toga" />
+        <img src="../cosplay/togaochaco3.png" alt="toga and ochaco" />
+        <img src="../cosplay/ochacomain.png" alt="group shot" />
+        <img src="../cosplay/ochaco5.png" alt="ochaco" />
+        <img src="../cosplay/mera1.png" alt="mera" />
+        <img src="../cosplay/mera4.png" alt="mera" />
+        <img src="../cosplay/mera6.png" alt="mera" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Images',
+}
+</script>
