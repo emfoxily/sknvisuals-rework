a5d62777f8af035eeae33cee154f67261dc8a430
emily reno <emilyreno@emilys-MacBook-Pro.local>
2019-01-10 11:20:59 -0500

    mobile katsu booking done

--- src/booking/Katsu.vue
+++ src/booking/Katsu.vue
@@ -1,9 +1,25 @@
 <template>
-    
+    <div>
+        <mq-layout mq="sm">
+            <Nav style="margin-top: 30vw" />
+            <iframe class="sm-iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSdSq6Foz-rJ-Cg5rtIBINzbUlieSfq_nbbxQt-DnZVBdbOmMA/viewform?embedded=true" width="305" height="600vw" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+        </mq-layout>
+    </div>
 </template>
 
 <script>
+import Nav from '../nav/Nav.vue'
+
 export default {
     name: 'Katsu',
+    components: {
+        Nav
+    }
 }
 </script>
+
+<style>
+    iframe.sm-iframe {
+        width: 100%;
+    }
+</style>
